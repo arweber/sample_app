103586d28b4277fc4469981ff80e2276c4ddaf2e
Andrew Weber <arweber@lbl.gov>
2012-04-19 10:06:14 -0400

    Formatted webpage

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -45,4 +45,17 @@ end
 
 end
 
+  describe "Get 'Help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+			:content => "Ruby on Rails Tutorial Sample App | Help")
+    end
+end
+
 end
